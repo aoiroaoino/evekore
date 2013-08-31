6b33845ce465c8c3b42d0fe90cd693f52269e65c
jomjomni <tlubtlub@gmail.com>
2013-08-31 14:08:28 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Evekore::Application.config.secret_key_base = 'e1e07b0481d7bdd2e0fe69b05abe9a5a4f45467c7ee33a1b6fd687e0523a6554d41c37ff998df1438d5fb34955f362bb8804e2336d1d87b9653f0c2e9f81138d'
