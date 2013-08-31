506f04bd2294c297db1546de81b2d9423fec2916
jomjomni <tlubtlub@gmail.com>
2013-08-31 20:47:01 +0900

    pre

--- app/helpers/mongo_api.rb
+++ app/helpers/mongo_api.rb
@@ -2,7 +2,14 @@ require 'json'
 require_relative '../models/mongodb'
 
 class MongoApi
-  mongodb = Mongodb.new
 
-  mongodb.get_collection_all
+  def initialize
+    mongodb = Mongodb.new
+
+    @viewJson = mongodb.get_collection_all
+  end
+
+  def getResult
+    @viewJson
+  end
 end
