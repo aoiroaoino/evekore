5cd69b5aea72d3abedff84be4bfac8bae5b5cd5f
Aoino <aoiro.aoino@gmail.com>
2013-08-31 21:13:27 +0900

    ver 1.5

--- app/controllers/evekore_controller.rb
+++ app/controllers/evekore_controller.rb
@@ -1,7 +1,16 @@
 class EvekoreController < ApplicationController
+  require 'apiAll'
+  require 'mongo_api'
 
   def index
-  	
+  	# api = ApiAll.new
+  	api = MongoApi.new
+  	@events = api.getResult.first["event"].sort{|f,s| f["start_time"].split("\s")[0] <=> s["start_time"].split("\s")[0]
+  	  }.sort{|f,s| f["start_time"].split("\s")[1] <=> s["start_time"].split("\s")[1]}
+  end
+
+  def time_sort
+  	@events.sort{|f,s| f.first <=> s.first}.sort{|f,s| f[1] <=> s[1]} unless str.nil?
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  
+  def table_color(event_site)
+  	case event_site
+  	when "ATND"
+  	  "error"
+  	when "Partake"
+  	  "info"
+  	when "DoorKeeper"
+  	  "success"
+  	else
+  	  "default"
+	end	
+  end
+
 end
